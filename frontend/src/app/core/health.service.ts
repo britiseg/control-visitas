e743c51b5e06ca889c112540b5325de2f7c94656
Brigette Gutierrez <britise@gmail.com>
2025-08-12 17:15:46 -0600

    chore: add Angular/Nest projects into monorepo

---
+++ frontend/src/app/core/health.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment'; // ajusta la ruta si usas el estándar
+
+@Injectable({ providedIn: 'root' })
+export class HealthService {
+  constructor(private http: HttpClient) {}
+  ping() {
+    return this.http.get(`${environment.apiUrl}/`);
+  }
+}
